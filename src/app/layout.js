5f0ff43bf795f597bd9d57c8e9f7e22048e6dfc5
businessnexflow-sketch <business.nexflow@gmail.com>
2026-04-29 01:26:00 +0400

    add google site verification meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import Providers from '@/components/Providers';
 
 export const metadata = {
   title: 'VertexFlix - Watch Movies & TV Shows',
@@ -20,11 +21,13 @@ export default function RootLayout({ children }) {
         <meta name="google-site-verification" content="Nuj4a41GCUU167KjjERsFDRrKreTNeipIfZdd5j9BjA" />
       </head>
       <body className="min-h-screen flex flex-col">
-        <Navbar />
-        <main className="flex-1 pt-16">
-          {children}
-        </main>
-        <Footer />
+        <Providers>
+          <Navbar />
+          <main className="flex-1 pt-16">
+            {children}
+          </main>
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
